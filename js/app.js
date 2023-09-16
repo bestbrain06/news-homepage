6823d31e3ebff38c3ccb92e5bd010f3a98f70256
bestbrain06 <136890174+bestbrain06@users.noreply.github.com>
2023-09-16 04:55:37 +0000

    completed most of the styles left with some finsihing touches

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const navigation = document.querySelector(".navigation__links");
+const navigationToggle = document.querySelector(".hamburger");
+
+navigationToggle.addEventListener("click", () => {
+  const visibility = navigation.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    navigation.setAttribute("data-visible", true);
+    navigationToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    navigation.setAttribute("data-visible", false);
+    navigationToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+console.log("hello");
